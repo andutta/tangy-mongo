5a195f513961353d6f338070b6ae8015d977108f
Anshuman Dutta <andutta@gmail.com>
2015-01-18 23:31:06 -0600

    removing gradle build

--- dockerimage.sh
+++ dockerimage.sh
@@ -1,3 +1,2 @@
-./gradlew clean build
-docker build -t andutta/tangy-mongo-1.0 .
-docker push andutta/tangy-mongo-1.0
+sudo docker build -t andutta/tangy-mongo-1.0 .
+sudo docker push andutta/tangy-mongo-1.0
